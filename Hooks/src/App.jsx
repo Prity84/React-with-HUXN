c268a7883eada60cf9d44dec137ac1c553e017c4
prity <pritykushwaha51@gmail.com>
2024-06-18 10:09:36 +0530

    hooks

---
+++ Hooks/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <>
+      <h1>Hooks</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, cumque.</p>
+    </>
+  );
+}
+
+export default App;
