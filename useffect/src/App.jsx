d2fc9a41fcf1e38280e4d255ebeb8162aff81f6a
prity <pritykushwaha51@gmail.com>
2024-06-20 10:30:06 +0530

    useEffect example

--- useffect/src/App.jsx
+++ useffect/src/App.jsx
@@ -1,13 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0);
 
+  //Without dependencies render again and again
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setCount((count) => count + 1);
+  //   }, 2000);
+  // });
 
-  useEffect(cb,dependencies)
+  //With dependencies render once
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setCount((count) => count + 1);
+  //   }, 2000);
+  // },[]);
+
+  //with variable
+
+  useEffect(() => {
+      setTimeout(() => {
+        setCount((count) => count + 1);
+      }, 2000);
+    },[count]);
+  
+
   return (
     <>
       <h1>Use-Effect Hook</h1>
-      <h2></h2>
+      <h2>I have rendered {count} times.</h2>
     </>
   );
 };
