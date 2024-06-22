f0bdfbc920848ca561ee712357571df96547a886
prity <pritykushwaha51@gmail.com>
2024-06-22 12:33:50 +0530

    usecallback

---
+++ useCallback - Copy (2)/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    console.log("Header rendered")
+  return (
+    <div>
+      <h2>Header</h2>
+    </div>
+  )
+}
+
+export default Header
--- useCallback/src/App.jsx
+++ useCallback/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Header from "./components/Header";
 
-
 function App() {
- 
-const[counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
 
+  //usecallback not create another fun cache fun in memory store
+  const newFn = useCallback(() => {}, []);
 
-  return(
-   <>
-   <Header />
-   <h1>{counter}</h1>
-   <button onClick={()=>{setCounter(prev=>prev+1)}}>Click here</button>
-  </>
-  )
+  return (
+    <>
+      <Header newFn={newFn} />
+      <h1>{counter}</h1>
+      <button
+        onClick={() => {
+          setCounter((prev) => prev + 1);
+        }}
+      >
+        Click here
+      </button>
+    </>
+  );
 }
 
 export default App;
